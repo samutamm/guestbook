b323ce32ca1cd2bb620dff43dc7fba8c61d7f42b
samutamm <samutamm@gmail.com>
2015-06-15 17:31:36 +0300

    initial commit

---
+++ src/guestbook/handler.clj
@@ -0,0 +1,27 @@
+(ns guestbook.handler
+  (:require [compojure.core :refer [defroutes routes]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.file-info :refer [wrap-file-info]]
+            [hiccup.middleware :refer [wrap-base-url]]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [guestbook.routes.home :refer [home-routes]]
+            [guestbook.models.db :as db]
+            [guestbook.routes.auth :refer [auth-routes]]))
+
+(defn init []
+  (println "guestbook is starting")
+  (if-not (.exists (java.io.File. "./db.sq3"))
+    (db/create-guestbook-table)))
+
+(defn destroy []
+  (println "guestbook is shutting down"))
+
+(defroutes app-routes
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (-> (routes auth-routes home-routes app-routes)
+      (handler/site)
+      (wrap-base-url)))
---
+++ src/guestbook/routes/auth.clj
@@ -0,0 +1,29 @@
+(ns guestbook.routes.auth
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [guestbook.views.layout :as layout]
+            [hiccup.form :refer
+              [form-to label text-field password-field submit-button]]
+            [noir.response :refer [redirect]]))
+
+
+(defn control [field name text]
+  (list (label name text)
+        (field name)
+        [:br]))
+
+(defn registration-page []
+  (layout/common
+    (form-to [:post "/register"]
+      (control text-field :id "screen name")
+      (control password-field :pass "Password")
+      (control password-field :pass1 "Retype Password")
+      (submit-button "Create Account"))))
+
+(defroutes auth-routes
+  (GET "/register" [_] (registration-page))
+  (POST "/register" [id pass pass1]
+    (if (= pass pass1)
+      (redirect "/")
+      (registration-page))))
+
+;; sivu 56 managing sessions
